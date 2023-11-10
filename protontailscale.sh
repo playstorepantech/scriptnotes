262abe7c0ca0bc476872c664c3611fbef23743a0
playstorepantech <91266091+playstorepantech@users.noreply.github.com>
2023-11-10 13:33:19 +0300

    Update protontailscale.sh

--- protontailscale.sh
+++ protontailscale.sh
@@ -2,6 +2,7 @@
 
 # Download ProtonVPN package
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3-2_all.deb
+dpkg -i protonvpn-stable-release_1.0.3-2_all.deb
 
 # Update package list
 apt-get update
